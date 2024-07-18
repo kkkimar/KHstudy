6d1013fc8f10d4386f11bf9fe2323680ff71849e
kkkimar <155516548+kkkimar@users.noreply.github.com>
2024-07-18 12:25:50 +0900

    [level 0] Title: 주사위의 개수, Time: 0.03 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int[] box, int n) {
+        int answer = 0;
+        int countX = box[0]/n;
+        int countY = box[1]/n;
+        int countZ = box[2]/n;
+
+        return countX*countY*countZ;
+    }
+}
